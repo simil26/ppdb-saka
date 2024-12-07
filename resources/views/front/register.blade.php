71e2e02c555ee82f8875e35fdefb9431c384ae30
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-12-07 10:43:56 +0700

    create register form page using template from admin lte

---
+++ resources/views/front/register.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.front.auth')
+
+@section('content')
+    <div class="register-box">
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="{{ url('') }}" class="h1"><b>PPDB</b> SAKA</a>
+            </div>
+            <div class="card-body">
+                <p class="login-box-msg">Daftar akun baru</p>
+
+                <form action{{ route('register.store') }}" method="post">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="name" placeholder="Full name">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="email" class="form-control" name="email" placeholder="Email">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="confirm_password" placeholder="Retype password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="agreeTerms" name="terms" name="terms" value="1">
+                                <label for="agreeTerms">
+                                    I agree to the <a href="#">terms</a>
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">
+                                Daftar
+                            </button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <div class="social-auth-links text-center">
+                    <a href="{{ route('login') }}" class="btn btn-block btn-primary">
+                        <i class="fas fa-sign-in-alt"></i>
+                        Sign in here!
+                    </a>
+                </div>
+
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
+    </div>
+    <!-- /.login-box -->
+@endsection
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('#show-password').on('change', function() {
+                if ($(this).is(':checked')) {
+                    $('input#password').attr('type', 'text');
+                } else {
+                    $('input#password').attr('type', 'password');
+                }
+            });
+        });
+    </script>
+@endpush
