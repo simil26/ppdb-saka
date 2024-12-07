484eb39a9a4d5dbebfbdd5f03d4213f54d10a5a9
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-12-07 11:11:44 +0700

    create store method for handling store request

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class RegisterController extends Controller
 {
@@ -18,7 +19,33 @@ class RegisterController extends Controller
     }
     public function register(Request $request)
     {
-        $data = $request->all();
-        dd($data);
+
+        $rules = [
+            'name' => 'required|min:3|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8',
+            'confirm_password' => 'required|same:password',
+        ];
+
+        $messages = [
+            'name.required' => 'Nama harus diisi',
+            'name.max' => 'Nama maksimal 255 karakter',
+            'email.required' => 'Email harus diisi',
+            'email.email' => 'Email tidak valid',
+            'email.max' => 'Email maksimal 255 karakter',
+            'email.unique' => 'Email sudah terdaftar',
+            'password.required' => 'Password harus diisi',
+            'password.min' => 'Password minimal 8 karakter',
+            'confirm_password.required' => 'Konfirmasi password harus diisi',
+            'confirm_password.same' => 'Konfirmasi password tidak sama dengan kolom password',
+        ];
+        try {
+            $credentials = $request->validate($rules, $messages);
+            $result = User::create($credentials);
+            return redirect()->route('login')->with('registerSuccess', 'Pendaftaran akun berhasil, silahkan login untuk mendaftar PPDB');
+        } catch (\Exception $e) {
+            // dd($e->getMessage());
+            return redirect()->back()->withInput()->withInput()->with('registerError', $e->getMessage());
+        }
     }
 }
