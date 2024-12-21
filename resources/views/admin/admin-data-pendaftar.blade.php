23dcf7e0db57f554330163aa6ca51906c86cb00b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-12-21 18:35:54 +0700

    added modal to modify regristrant data

--- resources/views/admin/admin-data-pendaftar.blade.php
+++ resources/views/admin/admin-data-pendaftar.blade.php
@@ -65,7 +65,7 @@
                                                 {{ $pendaftar->asal_sekolah }}
                                             </td>
                                             <td>
-                                                <button class="btn btn-warning">
+                                                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modal-default">
                                                     <i class="fas fa-edit"></i>
                                                     Ubah Data
                                                 </button>
@@ -86,6 +86,27 @@
 @endsection
 
 @push('scripts')
+    <div class="modal fade" id="modal-default">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Default Modal</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>One fine body&hellip;</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
     <script>
         $('#table-data-pendaftar').DataTable();
     </script>
