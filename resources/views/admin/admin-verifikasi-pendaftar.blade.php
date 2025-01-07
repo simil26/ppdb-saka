c2a0c84ee82bba8313a94f039be2794d72394c09
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-01-07 22:37:21 +0700

    change Verifikasi Pendaftar to Data Pendaftar

--- resources/views/admin/admin-verifikasi-pendaftar.blade.php
+++ resources/views/admin/admin-verifikasi-pendaftar.blade.php
@@ -8,12 +8,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Verifikasi Pendaftar</h1>
+                        <h1 class="m-0">Data Pendaftar</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Administrator</a></li>
-                            <li class="breadcrumb-item active">Verifikasi Pendaftar</li>
+                            <li class="breadcrumb-item active">Data Pendaftar</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
