2fd30c9bbf5ee63fa2884a48e21f0cf5fee05e3d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-01-11 21:14:14 +0700

    create method for store uploaded files

--- app/Http/Controllers/User/UploadFilesController.php
+++ app/Http/Controllers/User/UploadFilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\DokumenPendafataran;
 use Illuminate\Http\Request;
 
 class UploadFilesController extends Controller
@@ -22,7 +23,39 @@ class UploadFilesController extends Controller
 
     public function store(Request $request)
     {
-        $files = $request->all();
-        dd($files);
+        $noregPPDB = $request->input('noreg_ppdb');
+        $files = [
+            'ijazah' => $request->file('ijazah') ? $request->file('ijazah') : '-',
+            'kk' => $request->file('kk') ? $request->file('kk') : '-',
+            'akte' => $request->file('akte') ? $request->file('akte') : '-',
+            'ktp' => $request->file('ktp') ? $request->file('ktp') : '-',
+            'kip' => $request->file('kip') ? $request->file('kip') : '-',
+            'kis' => $request->file('kis') ? $request->file('kis') : '-',
+            'kks' => $request->file('kks') ? $request->file('kks') : '-',
+            'kps' => $request->file('kps') ? $request->file('kps') : '-',
+            'pkh' => $request->file('pkh') ? $request->file('pkh') : '-',
+        ];
+
+        try {
+            foreach ($files as $key => $file) {
+                if ($file) {
+                    $file->move(public_path('assets/files/' . $noregPPDB . '/'), $noregPPDB . '-' . $key . '.' . $file->extension());
+                }
+            }
+            $dokumenPPDB = DokumenPendafataran::create([
+                'noreg_ppdb' => $noregPPDB,
+                'ijazah' => $noregPPDB . '-ijazah.' . $files['ijazah']->extension(),
+                'kk' => $noregPPDB . '-kk.' . $files['kk']->extension(),
+                'akte' => $noregPPDB . '-akte.' . $files['akte']->extension(),
+                'ktp' => $noregPPDB . '-ktp.' . $files['ktp']->extension(),
+                'kip' => $noregPPDB . '-kip.' . $files['kip']->extension(),
+                'kis' => $noregPPDB . '-kis.' . $files['kis']->extension(),
+                'kks' => $noregPPDB . '-kks.' . $files['kks']->extension(),
+                'kps' => $noregPPDB . '-kps.' . $files['kps']->extension(),
+                'pkh' => $noregPPDB . '-pkh.' . $files['pkh']->extension(),
+            ]);
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Gagal mengunggah file. Silahkan coba lagi.');
+        }
     }
 }
