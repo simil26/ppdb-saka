37d0677ed8b3ed3516e04af175914c5ed22556da
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-01-11 19:05:29 +0700

    change user name with name from session

--- resources/views/layouts/admin/components/navbar.blade.php
+++ resources/views/layouts/admin/components/navbar.blade.php
@@ -41,7 +41,7 @@
                 <i class="far fa-user"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">Nama User</span>
+                <span class="dropdown-item dropdown-header">{{ session('name') }}</span>
                 <div class="dropdown-divider"></div>
                 <a href="#" class="dropdown-item">
                     <i class="fas fa-user mr-2"></i>
