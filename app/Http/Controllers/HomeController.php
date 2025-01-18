63c605cfabb4d9d2ecaee72b1e28fcaa6783f76f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-01-18 09:53:06 +0700

    added comment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    // Fungsi index selalu menjadi fungsi utama yg dipanggil untuk menampilkan halaman
     public function index()
     {
+        //Membuat variable dengan nama data dengan tipe associative array
+        //Associative array adalah array yg memiliki key/kata kunci dan value/isi, contoh "kata_kunci => isi"
         $data = [
             'page_title' => 'Portal',
             'usingDatatables' => false,
         ];
 
-        // dd(compact('data'));
-
+        //"return" adalah fungsi bawaan php untuk mengembalikan sesuatu, untuk laravel bisa mengembalikan view dan data
+        //"view" adalah fungsi bawaan laravel untuk menampilkan halaman, contoh "view('home', $data)"
         return view('home', $data);
     }
 }
--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -14,14 +14,22 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        //fungsi if ini untuk mengecek apakah session email sudah ada atau belum
+        //jika sudah berarti user sudah login dan akan diarahkan ke halaman dashboard
+        //jika belum berarti user belum login dan akan diarahkan kembali ke halaman login
         if (!session()->has('email')) {
             return redirect()->route('login');
         }
+
+        //5 variable dibawah ini digunakan untuk mengambil data dari database
+        //data ini digunakan untuk melakukan pengecekan data yang sudah diisi oleh user atau belum, untuk digunakan pada komponen status pendaftaran
         $biodata = Biodata::where('user_id', auth()->user()->id)->first();
         $dataOrangTua = DataOrangTua::where('user_id', auth()->user()->id)->first();
         $dataPeriodik = DataPeriodik::where('user_id', auth()->user()->id)->first();
         $dataKesejahteraan = DataKesejahteraan::where('user_id', auth()->user()->id)->first();
         $uploadFiles = DokumenPendaftaran::where('user_id', auth()->user()->id)->first();
+
+        //data dari 5 variable diatas akan dikirimkan ke view dashboard.blade.php melalui variable $data
         $data = [
             'title' => 'Dashboard',
             'active' => 'dashboard',
@@ -32,9 +40,18 @@ class DashboardController extends Controller
             'uploadFiles' => $uploadFiles
         ];
 
+        //fungsi if ini digunakan untuk mengecek apakah data diri sudah diisi atau belum
+        //jika sudah diisi maka akan menyimpan data noreg_ppdb ke dalam session
+        //noreg_ppdb dibutuhkan untuk mengirim data pada setiap kolom input di semua menu, agar data yg di inputkan terkunci sesuai dengan noreg_ppdb calon siswa
         if ($biodata) {
             session()->put('noreg_ppdb', $biodata->noreg_ppdb);
         }
+
+        //fungsi if ini digunakan untuk mengecek apakah dokumen pendaftaran sudah di unggah atau belum
+        //jika sudah di unggah maka akan membuat session uploadFiles dengan value selesai
+        //session ini digunakan untuk pengecekan pada menu upload file
+        //jika sudah di unggah maka url menu upload file akan diarahkan ke halaman yg menampilkan dokumen yg sudah di unggah
+        //jika belum di unggah maka url menu upload file akan diarahkan ke halaman yg berisi form upload file
         if ($uploadFiles) {
             session()->put('uploadFiles', 'selesai');
         }
