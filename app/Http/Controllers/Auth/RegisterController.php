2460f038b870276a328a34472450b6d2f0e0a0a7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-05-11 23:14:26 +0700

    modified register controller with adding some line for generate noreg_ppdb and upload payment receipt

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -25,10 +25,12 @@ class RegisterController extends Controller
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:8',
             'confirm_password' => 'required|same:password',
+            'bukti_bayar' => 'required|mimes:jpg,jpeg,png|max:2048',
         ];
 
         $messages = [
             'name.required' => 'Nama harus diisi',
+            'name.min' => 'Nama minimal 3 karakter',
             'name.max' => 'Nama maksimal 255 karakter',
             'email.required' => 'Email harus diisi',
             'email.email' => 'Email tidak valid',
@@ -38,11 +40,36 @@ class RegisterController extends Controller
             'password.min' => 'Password minimal 8 karakter',
             'confirm_password.required' => 'Konfirmasi password harus diisi',
             'confirm_password.same' => 'Konfirmasi password tidak sama dengan kolom password',
+            'bukti_bayar.required' => 'Wajib mengupload bukti bayar',
+            'bukti_bayar.mimes' => 'Bukti bayar harus berupa file jpg, jpeg, png',
+            'bukti_bayar.max' => 'Bukti bayar maksimal 2MB',
         ];
         try {
+            //Membuat nomor pendaftaran PPDB
+            $noreg_ppdb = 'PPDB-' . date('Ymd') . rand(1000, 9999);
+            //Cek apakah nomor pendaftaran sudah ada di database
+            while ($noreg_ppdb == User::where('noreg_ppdb', $noreg_ppdb)->first()) {
+                //Jika sudah ada, buat nomor pendaftaran baru
+                $noreg_ppdb = 'PPDB-' . date('Ymd') . rand(1000, 9999);
+            }
+
+            //Validasi data yg diinputkan
             $credentials = $request->validate($rules, $messages);
+            //Menambahkan nomor pendaftaran ke data yg diinputkan
+            $credentials['noreg_ppdb'] = $noreg_ppdb;
+            //Menambahkan nama file bukti bayar ke data yg diinputkan
+            $credentials['bukti_bayar'] = $noreg_ppdb . '-bukti_bayar.' . $request->file('bukti_bayar')->extension();
+            //Mengambil file bukti bayar dari form input
+            $fileBuktiBayar = $request->file('bukti_bayar');
+            //Memindahkan file bukti bayar ke folder assets/files/noreg_ppdb
+            $fileBuktiBayar->move(public_path('assets/files/' . $noreg_ppdb . '/'), $credentials['bukti_bayar']);
+
+            //Menyimpan data ke database
             $result = User::create($credentials);
-            return redirect()->route('login')->with('registerSuccess', 'Pendaftaran akun berhasil, silahkan login untuk mendaftar PPDB');
+            //Jika berhasil, simpan data pengguna ke session lalu diarahkan ke halaman dashboard
+            $request->session()->put('noreg_ppdb', $result->noreg_ppdb);
+
+            return redirect()->route('user.dashboard')->with('loginSession', 'Login berhasil');
         } catch (\Exception $e) {
             // dd($e->getMessage());
             return redirect()->back()->withInput()->withInput()->with('registerError', $e->getMessage());
