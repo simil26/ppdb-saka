8fe881c1674cd261bb65adc04ad5185dc048f13d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-05-11 23:15:48 +0700

    remove regenerate session for registrant status

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -17,17 +17,17 @@ class DashboardController extends Controller
         //fungsi if ini untuk mengecek apakah session email sudah ada atau belum
         //jika sudah berarti user sudah login dan akan diarahkan ke halaman dashboard
         //jika belum berarti user belum login dan akan diarahkan kembali ke halaman login
-        if (!session()->has('email')) {
+        if (!session()->has('noreg_ppdb')) {
             return redirect()->route('login');
         }
 
         //5 variable dibawah ini digunakan untuk mengambil data dari database
         //data ini digunakan untuk melakukan pengecekan data yang sudah diisi oleh user atau belum, untuk digunakan pada komponen status pendaftaran
-        $biodata = Biodata::where('user_id', auth()->user()->id)->first();
-        $dataOrangTua = DataOrangTua::where('user_id', auth()->user()->id)->first();
-        $dataPeriodik = DataPeriodik::where('user_id', auth()->user()->id)->first();
-        $dataKesejahteraan = DataKesejahteraan::where('user_id', auth()->user()->id)->first();
-        $uploadFiles = DokumenPendaftaran::where('user_id', auth()->user()->id)->first();
+        $biodata = Biodata::where('noreg_ppdb', session('noreg_ppdb'))->first();
+        $dataOrangTua = DataOrangTua::where('noreg_ppdb', session('noreg_ppdb'))->first();
+        $dataPeriodik = DataPeriodik::where('noreg_ppdb', session('noreg_ppdb'))->first();
+        $dataKesejahteraan = DataKesejahteraan::where('noreg_ppdb', session('noreg_ppdb'))->first();
+        $uploadFiles = DokumenPendaftaran::where('noreg_ppdb', session('noreg_ppdb'))->first();
 
         //data dari 5 variable diatas akan dikirimkan ke view dashboard.blade.php melalui variable $data
         $data = [
@@ -39,13 +39,6 @@ class DashboardController extends Controller
             'dataKesejahteraan' => $dataKesejahteraan,
             'uploadFiles' => $uploadFiles
         ];
-
-        //fungsi if ini digunakan untuk mengecek apakah data diri sudah diisi atau belum
-        //jika sudah diisi maka akan menyimpan data noreg_ppdb ke dalam session
-        //noreg_ppdb dibutuhkan untuk mengirim data pada setiap kolom input di semua menu, agar data yg di inputkan terkunci sesuai dengan noreg_ppdb calon siswa
-        if ($biodata) {
-            session()->put('noreg_ppdb', $biodata->noreg_ppdb);
-        }
 
         //fungsi if ini digunakan untuk mengecek apakah dokumen pendaftaran sudah di unggah atau belum
         //jika sudah di unggah maka akan membuat session uploadFiles dengan value selesai
