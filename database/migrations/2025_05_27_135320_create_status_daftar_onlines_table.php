d8859fbf7f4a9fcf6100d2432a32bbbac66ef91b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-05-27 21:28:48 +0700

    add some fields column

--- database/migrations/2025_05_27_135320_create_status_daftar_onlines_table.php
+++ database/migrations/2025_05_27_135320_create_status_daftar_onlines_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('status_daftar_onlines', function (Blueprint $table) {
             $table->id();
+            $table->string('noreg_ppdb')->unique();
+            $table->enum('statusBiodata', [0, 1])->default(0);
+            $table->enum('statusDataOrangTua', [0, 1])->default(0);
+            $table->enum('statusDataPeriodik', [0, 1])->default(0);
+            $table->enum('statusKesejahteraan', [0, 1])->default(0);
+            $table->enum('statusDokumenPendaftaran', [0, 1])->default(0);
+            $table->enum('statusFinalisasi', [0, 1])->default(0);
             $table->timestamps();
         });
     }
