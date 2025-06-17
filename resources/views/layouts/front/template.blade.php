1c98789ac01fe174064ea3318b07013b36875716
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2025-06-17 18:34:25 +0700

    modified page title

--- resources/views/layouts/front/template.blade.php
+++ resources/views/layouts/front/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $page_title }} | PPDB Sekolah Alam Karawang</title>
+    <title>{{ $page_title }} | PPDB SD Alam Amani Karawang</title>
     <link rel="shortcut icon" href="{{ url('assets/img/favicon.webp') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ url('assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
