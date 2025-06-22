8d947ef8846c0e01e027dcc8b1c15f28a13503b5
Muhamad Ramdani Hidayatullah <muhamadramdani.tmkm@gmail.com>
2025-06-22 23:08:06 +0700

    create new method for download registrant files

--- app/Http/Controllers/Admin/HasilSeleksiController.php
+++ app/Http/Controllers/Admin/HasilSeleksiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Biodata;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\DokumenPendaftaran;
 
 class HasilSeleksiController extends Controller
 {
@@ -22,5 +23,42 @@ class HasilSeleksiController extends Controller
         ];
 
         return view('admin.hasil-seleksi', $data);
+    }
+
+    public function unduhDokumen($noreg_ppdb)
+    {
+        $zipArchiver = new \ZipArchive();
+        $zipFileName = 'dokumen-pendaftaran-' . $noreg_ppdb . '.zip';
+        $fileCollection = DokumenPendaftaran::where('noreg_ppdb', $noreg_ppdb)->first();
+        $zipFilePath = public_path('assets/files/' . $zipFileName);
+        if ($zipArchiver->open($zipFilePath, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) === true) {
+            $files = [];
+            if ($fileCollection) {
+                foreach ($fileCollection->getAttributes() as $key => $value) {
+                    if ($value !== '-') {
+                        if (gettype($value) !== 'integer') {
+                            $files[] = $value;
+                        }
+                    }
+                }
+                array_splice($files, 0, 1);
+                array_pop($files); // Remove 'noreg_ppdb' from the list
+                array_pop($files); // Remove 'noreg_ppdb' from the list
+            }
+
+            foreach ($files as $file) {
+                $filePath = public_path('assets/files/' . $noreg_ppdb . '/' . $file);
+                if (is_dir($filePath)) {
+                    $zipArchiver->addEmptyDir($file);
+                    $this->addFilesToZip($zipArchiver, $filePath, $file);
+                } elseif (is_file($filePath)) {
+                    $zipArchiver->addFile($filePath, $file);
+                }
+            }
+
+            $zipArchiver->close();
+
+            return response()->download($zipFilePath)->deleteFileAfterSend(true);
+        }
     }
 }
