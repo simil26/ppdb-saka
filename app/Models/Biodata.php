a469fece0d775326a04213cf4fd050a408481e6f
Muhamad Ramdani Hidayatullah <muhamadramdani.tmkm@gmail.com>
2025-06-27 22:22:44 +0700

    add new relations to get all the rest of all data

--- app/Models/Biodata.php
+++ app/Models/Biodata.php
@@ -9,4 +9,22 @@ class Biodata extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function dataOrangTua()
+    {
+        // Asumsi: 'noreg_ppdb' adalah foreign key di tabel 'data_orang_tua'
+        return $this->hasOne(DataOrangTua::class, 'noreg_ppdb', 'noreg_ppdb');
+    }
+
+    public function dataPeriodik()
+    {
+        // Asumsi: 'noreg_ppdb' adalah foreign key di tabel 'data_periodik'
+        return $this->hasOne(DataPeriodik::class, 'noreg_ppdb', 'noreg_ppdb');
+    }
+
+    public function dataKesejahteraan()
+    {
+        // Asumsi: 'noreg_ppdb' adalah foreign key di tabel 'data_kesejahteraan'
+        return $this->hasOne(DataKesejahteraan::class, 'noreg_ppdb', 'noreg_ppdb');
+    }
 }
